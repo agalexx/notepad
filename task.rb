f2e6cfe4dac11e8246978a6ff8895d72c8f606a0
agalex <mail@agalex.ru>
2017-02-01 21:04:50 +0300

    initial commit 4

--- task.rb
+++ task.rb
@@ -18,6 +18,12 @@ class Task < Post
   end
 
   def to_string
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+
+    deadline = "Крайний срок: #{@due_date}"
+    return [deadline, @text, time_string]
+
   end
 
 
